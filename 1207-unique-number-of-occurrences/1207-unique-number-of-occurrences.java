d33845e2716083abeebc893e85b349964ab747e5
dyvand22 <109952270+dyvand22@users.noreply.github.com>
2025-12-10 11:21:53 +0530

    Time: 3 ms (34.77%), Space: 43.6 MB (27.41%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,16 +1,13 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int x: arr) map.put(x,map.getOrDefault(x,0)+1);
+        HashSet<Integer> set = new HashSet<>(map.values());
+        if (set.size()==map.size()) return true;
+        else return false;
+        
 
-        // Step 1: count occurrences using HashMap
-        Map<Integer, Integer> freq = new HashMap<>();
-        for (int x : arr) {
-            freq.put(x, freq.getOrDefault(x, 0) + 1);
-        }
 
-        // Step 2: store frequencies in a HashSet
-        Set<Integer> set = new HashSet<>(freq.values());
-
-        // if sizes differ → duplicate frequencies exist
-        return set.size() == freq.size();
+        
     }
 }
