32e18a738d7a53bf925b78fec6b1c614807a822a
dyvand22 <109952270+dyvand22@users.noreply.github.com>
2025-12-18 18:48:27 +0530

    Time: 165 ms (6.92%), Space: 47.8 MB (6.15%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,36 +1,24 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
         HashSet<List<Integer>> set = new HashSet<>();
-
-        for (int i = 0; i < nums.length - 3; i++) {
-            for (int j = i + 1; j < nums.length - 2; j++) {
-                int left = j + 1;
-                int right = nums.length - 1;
-
-                while (left < right) {
-                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
-
-                    if (sum < target) {
-                        left++;
-                    } else if (sum > target) {
-                        right--;
-                    } else { 
-                        // found one
-                        set.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-
+        Arrays.sort(nums);
+      
+        for(int i = 0;i< nums.length-3;i++){
+            for(int j =i+1;j<nums.length-2;j++){
+                  int left = j+1;
+                  int right = nums.length-1;
+                while(left<right){
+                    long sum = (long)nums[i]+ nums[j]+nums[left]+nums[right];
+                    if(target==sum){
+                        set.add(Arrays.asList(nums[left],nums[right],nums[i],nums[j]));
                         left++;
                         right--;
-
-                        // skip duplicates for left
-                        while (left < right && nums[left] == nums[left - 1]) left++;
-
-                        // skip duplicates for right
-                        while (left < right && nums[right] == nums[right + 1]) right--;
                     }
+                    if(sum<target) left++;
+                    if(sum>target) right--;
                 }
             }
         }
-        return new ArrayList<>(set);
+        return new ArrayList(set);
     }
 }
