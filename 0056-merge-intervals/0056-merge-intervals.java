e610348caf98f517fb2b461c70d56c3267de64df
dyvand22 <109952270+dyvand22@users.noreply.github.com>
2025-12-20 15:52:12 +0530

    Time: 8 ms (88.66%), Space: 49.4 MB (7.33%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,20 +1,19 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        Stack<int[]> stack = new Stack<>();
-        stack.push(intervals[0]);
-
-        for(int i = 1;i<intervals.length;i++){
-            int [] last = stack.peek();
-            
-            if(intervals[i][0]<=last[1]){
-                last[1]=Math.max(last[1],intervals[i][1]);
-
-            }else{
-                stack.push(intervals[i]);
-            }
+       Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+       Stack<int[]> stack = new Stack<>();
+       stack.push(intervals[0]);
+       
+       for(int i = 1; i<intervals.length;i++){
+        int [] last = stack.peek();
+        if(last[1]>=intervals[i][0]){
+            last[1]=Math.max(last[1],intervals[i][1]);
+        }else{
+            stack.push(intervals[i]);
         }
-         return stack.toArray(new int[stack.size()][]);
+
+       }
+       return stack.toArray(new int[stack.size()][]);
 
         
     }
