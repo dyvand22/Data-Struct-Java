c0953d2c00928ae002f5f76e78664e396ad82c9c
dyvand22 <109952270+dyvand22@users.noreply.github.com>
2025-12-20 22:09:10 +0530

    Time: 2 ms (92.7%), Space: 72.4 MB (8.41%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,27 +1,41 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-       int zeroz = 0;
-       int product = 1;
-       int [] res = new int [nums.length];
-       for(int num : nums){
-        if(num==0) zeroz++;
-        else product *=num;
-       }
-       if(zeroz>1){
-            return res;
+
+        int zero = 0;
+        int product = 1;
+        int[] res = new int[nums.length];
+
+        for (int num : nums) {
+            if (num == 0) {
+                zero++;
+            } else {
+                product *= num;
+            }
         }
 
-       for(int i = 0;i<nums.length;i++){
-        if(zeroz ==0){
-            res[i]=product/nums[i];
+        // Case 1: more than one zero
+        if (zero > 1) {
+            return res; // all zeros
+        }
 
+        // Case 2: no zero
+        if (zero == 0) {
+            for (int i = 0; i < res.length; i++) {
+                res[i] = product / nums[i];
+            }
         }
-        else if (zeroz == 1){
-            if(nums[i]==0){
-                res[i] = product;
-            }else res[i]=0;
+
+        // Case 3: exactly one zero
+        if (zero == 1) {
+            for (int i = 0; i < res.length; i++) {
+                if (nums[i] == 0) {
+                    res[i] = product;
+                } else {
+                    res[i] = 0;
+                }
+            }
         }
-       }
-       return res;
+
+        return res;
     }
 }
